897e87a9bce7d734ce90dc0eb280e5fb0487aa38
frank <frankserranobasilio@gmail.com>
2019-06-29 13:14:47 -0500

    envio1

---
+++ database/migrations/2019_06_29_173452_create_alumnos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAlumnosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('alumnos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nombre',50);
+            $table->string('apellido',100);
+            $table->integer('celular');
+            $table->string('estado_civil');
+            $table->integer('dni');
+            $table->string('grado_inst');
+            $table->timestamps();
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('alumnos');
+    }
+}
